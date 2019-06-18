9e19edf2ac0ba24166f92a0beb6bbb8a62843312
1ambda <1amb4a@gmail.com>
2019-06-19 00:43:26 +0900

    feat(catalog-ui): Add App.scss

--- service-catalog-ui/src/App.vue
+++ service-catalog-ui/src/App.vue
@@ -1,33 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header>
+        <app-navbar></app-navbar>
+      </el-header>
+      <div>
+        <router-view></router-view>
+      </div>
+    </el-container>
   </div>
 </template>
 
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator'
+  import Navbar from '@/components/Navbar/Index.vue' // @ is an alias to /src
+
+  @Component({
+    components: {
+      'app-navbar': Navbar,
+    },
+  })
+  export default class App extends Vue {
+  }
+
+</script>
+
 <style lang="scss">
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-
-  #nav {
-    padding: 30px;
-
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
-  }
+  @import './App.scss';
 </style>
---
+++ service-catalog-ui/src/components/Navbar/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Navbar
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from 'vue-property-decorator'
+
+  @Component
+  export default class Navbar extends Vue {
+  }
+</script>
+
